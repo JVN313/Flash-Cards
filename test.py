458f0d0f6cbac80e9b59cb45b4709fa96dbf9e10
JVN313 <jvncovington@gmail.com>
2025-04-25 22:59:25 -0400

    began creating a note taking mode in the form of txt files

---
+++ test.py
@@ -0,0 +1,24 @@
+import glob
+
+def CreatePage():
+    page_title = input("Enter Name Of New Note Page:  ").upper()
+    page = open(f"{page_title}.txt","w")
+    page.close()
+    print(f"New File '{page_title}' created!\n")
+
+def ListNotes():
+    for i in glob.glob("*.txt"):
+        print(i)
+
+def EditFiles(name):
+    test = glob.glob("*.txt")
+    name = str(name) + ".txt"
+    working = True
+    while working:
+        if name not in test:
+            print("Sorry file does not exist!\n")
+            name = input("Enter Name Of File:  ").upper() + ".txt"
+        else:
+            print(name)
+            working = False
+    
