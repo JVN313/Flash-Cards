bb973650bb8ec28a560941d13172f2f0fbdf404f
JVN313 <jvncovington@gmail.com>
2025-04-27 16:19:33 -0400

    further improved the EditFiles function and updated the TODO file

--- test.py
+++ test.py
@@ -10,15 +10,29 @@ def ListNotes():
     for i in glob.glob("*.txt"):
         print(i)
 
-def EditFiles(name):
+def EditFiles(file_name):
     test = glob.glob("*.txt")
-    name = str(name) + ".txt"
+    file_name = str(file_name) + ".txt"
+    file_edit = ""
+    file_choice = ""
+    file_read = "r"
+    file_append = "a"
     working = True
+    
     while working:
-        if name not in test:
+        if file_name not in test:
             print("Sorry file does not exist!\n")
-            name = input("Enter Name Of File:  ").upper() + ".txt"
+            file_name = input("Enter Name Of File:  ").upper() + ".txt"
         else:
-            print(name)
+            print(f"Accessing File '{file_name}'!\n")
             working = False
     
+    file_choice = input("To Read The File Enter 'R'/To Add to file enter 'A':\n").lower()
+    if file_choice == "a":
+        file_edit = open(f"{file_name}", "a")
+        file_append = input("Enter What You Wish To add To THe file:\n")
+        file_edit.write(f"\n{file_append}")
+        file_edit.close()
+
+EditFiles("TEST NOTE")
+#Finishing The EditFiles function
